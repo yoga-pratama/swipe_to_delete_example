2d158b9485095d801bf70ea27c6a0623def8c6be
Yoga Pratama <yogamadness@gmail.com>
2024-02-05 11:58:59 +0700

    commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "swipe_to_delete_example"
+include(":app")
+ 
